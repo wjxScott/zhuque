dbfe69faec4f1df77de013785e866e3cfa3375a8
流沙 <liusha@wacai.com>
2021-05-05 21:55:43 +0800

    leetcode106

---
+++ src/main/java/com/wjx/zhuque/leetcode/leetcode106/Solution.java
@@ -0,0 +1,37 @@
+package com.wjx.zhuque.leetcode.leetcode106;
+
+
+import com.wjx.zhuque.leetcode.TreeNode;
+
+/**
+ * @author wangjinxin
+ * @date 2021/5/5 18:41
+ * @mail wjxScott@iCloud.com
+ * @description
+ */
+public class Solution {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        TreeNode root = build(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
+        return root;
+    }
+
+    private TreeNode build(int[] inorder, int inStart, int inEnd, int[] postorder, int postStart, int postEnd) {
+        if (postStart > postEnd) {
+            return null;
+        }
+
+        int rootVal = postorder[postEnd];
+        int index = -1;
+        for (int i = inStart; i <= inEnd; i++) {
+            if (rootVal == inorder[i]) {
+                index = i;
+            }
+        }
+
+        TreeNode root = new TreeNode(rootVal);
+        int leftSize = index - inStart;
+        root.left = build(inorder, inStart, index - 1, postorder, postStart, postStart + leftSize - 1);
+        root.right = build(inorder, index + 1, inEnd, postorder, postStart + leftSize, postEnd - 1);
+        return root;
+    }
+}
