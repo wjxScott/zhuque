888dfd0454229deacfb3b612c84298c1a3cb354a
流沙 <liusha@wacai.com>
2021-05-05 16:48:48 +0800

    leetcode114

---
+++ src/main/java/com/wjx/zhuque/leetcode/leetcode114/Solution.java
@@ -0,0 +1,34 @@
+package com.wjx.zhuque.leetcode.leetcode114;
+
+
+import com.wjx.zhuque.leetcode.TreeNode;
+
+/**
+ * @author wangjinxin
+ * @date 2021/5/5 16:31
+ * @mail wjxScott@iCloud.com
+ * @description
+ */
+public class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        flatten(root.left);
+        flatten(root.right);
+
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        root.left = null;
+        root.right = left;
+
+        TreeNode p = root;
+
+        while (p.right != null) {
+            p = p.right;
+        }
+        p.right = right;
+    }
+}
