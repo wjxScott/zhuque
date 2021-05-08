57f50e48c5448ae9ffc4ede2821058f70483f4f9
流沙 <liusha@wacai.com>
2021-05-08 13:03:20 +0800

    leetcode230

---
+++ src/main/java/com/wjx/zhuque/leetcode/leetcode230/Solution.java
@@ -0,0 +1,36 @@
+package com.wjx.zhuque.leetcode.leetcode230;
+
+
+import com.wjx.zhuque.leetcode.TreeNode;
+
+/**
+ * @author wangjinxin
+ * @date 2021/5/8 12:49
+ * @mail wjxScott@iCloud.com
+ * @description
+ */
+public class Solution {
+
+    private int rank;
+
+    private int result;
+
+    public int kthSmallest(TreeNode root, int k) {
+        reverse(root, k);
+        return result;
+    }
+
+    private void reverse(TreeNode root, int k) {
+        if (root == null) {
+            return;
+        }
+
+        reverse(root.left, k);
+        rank++;
+        if (rank == k) {
+            result = root.val;
+            return;
+        }
+        reverse(root.right, k);
+    }
+}
